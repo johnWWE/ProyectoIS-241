548bbe5c4362c8a24c617627abbb24c18cec814d
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-21 21:56:10 -0500

    main inicializado

---
+++ main.py
@@ -0,0 +1,23 @@
+while True:
+    print("\MENU")
+    print("""[1] producto de matrices
+[2] suma de matrices
+[3] resta
+[4] inversa
+[5] transpuesta
+[6] determinante
+[0] salir """)
+    print()
+    llenar =int(input("opcion :"))
+    if llenar == 1:
+        print("opcion 1")
+    #productoMatrices()
+    elif llenar == 2:
+        pass
+    elif llenar == 3:
+        pass
+    elif llenar ==0:
+        pass
+        break
+    else:
+        print('de nuevo:')
